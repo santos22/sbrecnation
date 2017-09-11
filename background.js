25711f5de739e5db6f810aff18a11d7ae33319c0
ssolorzano <santos_solorzano@intuit.com>
2017-09-10 19:44:59 -0700

    Functioning extension across blogs :smile: - need to properly handle end of comments

--- background.js
+++ background.js
@@ -5,17 +5,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   	var activeTab = tabs[0];
-    console.log("In background");
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
   });
 });
-
-// This block is new!
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "open_new_tab" ) {
-    	console.log("In new block");
-    	//chrome.tabs.create({"url": request.url});
-    }
-  }
-);
--- content.js
+++ content.js
@@ -1,31 +1,43 @@
 // content.js
+// https://stackoverflow.com/questions/7404544/jquery-count-number-of-divs-with-a-certain-class
+// https://robots.thoughtbot.com/how-to-make-a-chrome-extension
+
+var commentToScrollTo = 0;
+var identifier = "#";
+var firstRecComment;
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
 
       // GET ENTIRE LIST OF COMMENTS
       var rec = document.getElementsByClassName("c-comments__list");
-
-      console.log("Num of rec comments: " + rec.length);
+      firstRecComment = rec[0].getElementsByClassName('c-comments__comment c-comments__recommended')[0].id;
 
       var numDivs = $('.c-comments__recommended').length;
       console.log("Num of divs: " + numDivs);
 
       // GET ONLY RECOMMENDED COMMENTS
-      for (i = 0; i < numDivs; i++) { 
-        console.log(rec[0].getElementsByClassName('c-comments__comment c-comments__recommended')[i].id);
-      }
+      // for (i = 0; i < numDivs; i++) { 
+      //   console.log(rec[0].getElementsByClassName('c-comments__comment c-comments__recommended')[i].id);
+      // }
       
-      var comment = rec[0].getElementsByClassName('c-comments__comment c-comments__recommended')[2].id;
-      console.log(comment);
-
-      $('html, body').animate({
-          scrollTop: $("#442860141").offset().top
-      }, 2000);
-
-      // This line is new!
-      chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+      if(commentToScrollTo < numDivs) {
+        var recCommentId = rec[0].getElementsByClassName('c-comments__comment c-comments__recommended')[commentToScrollTo].id;
+        var result = identifier.concat(recCommentId);
+        
+        $('html, body').animate({
+            scrollTop: $(result).offset().top
+        }, 500);
+        commentToScrollTo++;
+      } else {
+        commentToScrollTo = 0;
+        var result = identifier.concat(firstRecComment);
+        $('html, body').animate({
+            scrollTop: $(result).offset().top
+        }, 500);
+        commentToScrollTo++;
+      }
+      console.log("Count" + commentToScrollTo);
     }
   }
 );
