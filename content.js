867d11f4ecb91b92bac2075e1b83b8160ef89ed6
ssolorzano <santos_solorzano@intuit.com>
2017-09-10 18:11:56 -0700

    Getting all divs - need to save div IDs

--- content.js
+++ content.js
@@ -6,27 +6,26 @@ chrome.runtime.onMessage.addListener(
 
       // GET ENTIRE LIST OF COMMENTS
       var rec = document.getElementsByClassName("c-comments__list");
+
       console.log("Num of rec comments: " + rec.length);
-      console.log(rec[0]);
+
+      var numDivs = $('.c-comments__recommended').length;
+      console.log("Num of divs: " + numDivs);
 
       // GET ONLY RECOMMENDED COMMENTS
-      var comment = rec[0].getElementsByClassName('c-comments__comment c-comments__recommended')[1].id;
+      for (i = 0; i < numDivs; i++) { 
+        console.log(rec[0].getElementsByClassName('c-comments__comment c-comments__recommended')[i].id);
+      }
+      
+      var comment = rec[0].getElementsByClassName('c-comments__comment c-comments__recommended')[2].id;
       console.log(comment);
 
       $('html, body').animate({
-        scrollTop: $("#416054535").offset().top
-    }, 2000);
-
-      //var IDs = [];
-      //$("#mydiv").find("span").each(function(){ IDs.push(this.id); });
+          scrollTop: $("#442860141").offset().top
+      }, 2000);
 
       // This line is new!
       chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
     }
   }
 );
-
-// $(function() {
-//     var recs = document.querySelector('div.c-comments__comment c-comments__recommended').innerHTML;
-//     console.log("Num of rec comments: " + recs);
-// });
